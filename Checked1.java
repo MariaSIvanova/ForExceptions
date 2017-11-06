f5500a3f7fb41d49cbf8fdff421d0471c86249f3
MariaSIvanova <ims.maria9@gmail.com>
2017-11-06 17:35:26 +0400

    add try/catch bloc

--- Checked1.java
+++ Checked1.java
@@ -5,6 +5,12 @@ public class Checked1 implements Runnable{
     public static void main (String[] args){
         Thread t = new Thread (new Checked1());
         t.run();
+        try {
+            Thread.sleep(2000);
+        }
+        catch (InterruptedException e){
+            System.out.println ("we catch InterruptedException");
+        }
     }
 
 }
